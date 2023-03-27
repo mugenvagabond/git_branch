c2a53d3fe20cd45d2e722865c531b37aabb48947
mugenvagabond <monkruus96@gmail.com>
2023-03-28 01:15:17 +0300

    Добавлена функция first_letter_title, которая получает на вход строку и выводит строку с первой буквой - заглавной

--- str_func.py
+++ str_func.py
@@ -2,3 +2,11 @@ def get_title(stroke):
     new_stroke = stroke.upper()
     return new_stroke
 
+
+def first_letter_title(value):
+    """
+    :param value: функция принимает на вход строку
+    :return: возвращает строку с первой заглавной буквой
+    """
+    title_stroke = value.title()
+    return title_stroke
